3fbf421e31324ad01a24dc74a7894715ca5c585f
Henry <henrytimmons2004@gmail.com>
2023-03-27 11:13:28 -0700

    Initial Commit

---
+++ CS2_Inheritance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS2_Inheritance
+//
+//  Created by Henry Timmons on 3/27/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
